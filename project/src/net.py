5f32860e8f9cafe8b42c7bb059866bf10e287920
Accoutant <13667972419@163.com>
2023-11-05 16:50:55 +0800

    因子分析

--- project/src/net.py
+++ project/src/net.py
@@ -4,6 +4,8 @@ import pickle
 import pandas as pd
 import numpy as np
 from sklearn.decomposition import PCA
+import xgboost as xgb
+from sklearn.metrics import mean_squared_error
 
 
 def use_linear(labels: list):
@@ -55,9 +57,36 @@ def use_pca():
     print(pca.score(x_test,t_test))
     print(x_pca.shape)
 
-labels = ["15分", "30分", "60分"]
-# predict = use_svm(labels)
-use_pca()
 
+def use_xgboost():
+    with open("train_data.pkl", "rb") as f:
+        (x_train, t_train), (x_test, t_test), valid_data = pickle.load(f)
+    dtrain = xgb.DMatrix(x_train, label=t_train)
+    dtest = xgb.DMatrix(x_test, label=t_test)
+    params = {
+        'max_depth': 4,  # 每棵决策树的最大深度
+        'eta': 0.1,  # 学习率
+        'subsample': 0.7,  # 每次随机选择的样本比例
+        'colsample_bytree': 0.7,  # 每棵决策树随机选择的特征比例
+        'objective': 'reg:squarederror',  # 损失函数
+        'eval_metric': 'rmse',  # 评价指标
+        'silent': 1  # 是否输出日志信息
+    }
+    # 训练 XGBoost 模型
+    num_round = 100  # 决策树的数量
+    bst = xgb.train(params, dtrain, num_round)
+
+    # 使用测试集进行预测
+    t_pred = bst.predict(dtest)
+    print('RMSE:', mean_squared_error(t_test, t_pred, squared=False))
+    predict = bst.predict(xgb.DMatrix(valid_data))
+    predict = pd.DataFrame(predict, columns=['15分', '30分', '60分'])
+    with open("predict.pkl", "wb") as f:
+        pickle.dump(predict, f)
+
+# labels = ["15分", "30分", "60分"]
+# predict = use_svm(labels)
+# use_pca()
+use_xgboost()
 
 
